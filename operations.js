88d80dce7a353552f0a523086e1dcc1d1ebd6441
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-04-04 22:28:31 +0200

    Updated to new mono

--- operations.js
+++ operations.js
@@ -1,35 +1,16 @@
-var send = require(CONFIG.root + "/core/send.js").send;
-var model = require(CONFIG.root + "/core/model/orient.js");
-var mongo = require("mongodb");
-var crypto = require("crypto");
-
-
-var dataSources = {
-    usersDS: {
-        type: "mongo",
-        db: "partnerlogin",
-        collection: "users"
-    },
-    adminsDS: {
-        type: "mongo",
-        db: "partnerlogin",
-        collection: "admins"
-    }
-}
-
-var databases = {};
+var crypto = require('crypto');
 
 
 exports.login = function(link) {
 
-    if (link.req.session.role) {
-        send.badrequest(link, "You are already logged in");
+    if (link.session._uid) {
+        link.send(400, 'You are already logged in');
         return;
     }
 
     var data = link.data;
     if (!data) {
-        send.badrequest(link, "Missing data");
+        link.send(400, 'Missing login data');
         return;
     }
 
@@ -44,9 +25,7 @@ exports.login = function(link) {
 
     if (!username || !password) {
         var errCode = username ? ERROR_MISSING_PASSWORD : ERROR_MISSING_USERNAME;
-        return error(errCode, function(err) {
-            send.badrequest(link, err.toString());
-        });
+        return link.send(400, error(errCode).message);
     }
 
     // TODO do some transformation on the username
@@ -56,62 +35,50 @@ exports.login = function(link) {
 
         if (err) {
             if (err.code) {
-                onError(link, err, function() {
-                    send.forbidden(link, err.toString());
+                onError(link, err, function(err) {
+                    link.send(403, err.toString());
                 });
                 return;
             }
-            send.badrequest(link, err);
+            link.send(400, err);
             return;
         }
 
-        // TODO read the users in other way???
-        //      probably with the removal of users from orient
-        var orientUser = user.gpmail ? "user" : "admin";
+        var data = {
+            cid: user.cid,
+            name: user.fullname
+        };
 
-        getOrientUser(link, orientUser, function(err) {
-
-            if (err) {
-                send.internalservererror(link, err);
-                return;
-            }
-
-            if (orientUser === "user") {
-                link.req.session.cid = user.cid;
-                link.req.session.role = user.domain + "@garageplus.ch";
-            } else {
-                link.req.session.cid = "admin";
-                link.req.session.role = username;
-            }
-
-            send.ok(link.res, {});
+        // TODO harcoded role and user IDs and locale
+        M.session.start(link, 2, 120, 'de', data, function(err, session) {
+            link.send(200, {});
         });
-    })
+    });
 };
 
-function onError(link, err, callback) {
+function onError(link, initialError, callback) {
 
     var handler = link.params.on.error;
 
     // no custom error handler specified
     if (!handler) {
-        return callback();
+        return callback(initialError);
     }
 
-    api_customCode(link.session.appid, handler, function(err1, foo) {
+    api_customCode(handler, function(err, foo) {
 
-        if (err1) {
-            return callback()
-        }
-        foo(link, err, callback);
+        if (err) { return callback(initialError) }
+
+        foo(link, initialError, callback);
     });
 }
 
-function api_customCode(appId, handler, callback) {
+function api_customCode(handler, callback) {
+
     try {
         var modulePath = handler["module"];
         var functionName = handler["function"];
-        var path = CONFIG.APPLICATION_ROOT + appId + "/" + modulePath;
+        var path = M.config.APPLICATION_ROOT + M.config.app.id + "/" + modulePath;
 
         // TODO do this only in debug mode
         //      even so it is still problematic it the module caches data in RAM
@@ -132,22 +99,6 @@ function api_customCode(appId, handler, callback) {
     }
 }
 
-function getOrientUser(link, username, callback) {
-
-    var appId = link.req.session.appid;
-
-    // TODO we only need role from mono
-    model.getUser(appId, username, function(err, user) {
-
-        if (err || !user) {
-            return callback(err || "Could not find user '" + username + "' for application: " + appId);
-        }
-
-        link.req.session.uid = user.uid;
-
-        callback(null);
-    });
-}
 
 function getUser(params, username, password, callback) {
 
@@ -155,59 +106,54 @@ function getUser(params, username, password, callback) {
         return callback(new Error("Missing operation parameters"));
     }
 
-    var dataSource = dataSources[params.ds];
-
-    openDatabase(dataSource, function(err, db) {
+    M.datasource.resolve(params.ds, function(err, ds) {
 
         if (err) { return callback(err); }
 
-        // callback to close the database
-        var oldCallback = callback;
-        callback = function(err, data) {
-            db.close(function() {
-                oldCallback(err, data);
-            });
-        }
-
-        db.collection(dataSource.collection, function(err, collection) {
+        M.database.open(ds, function(err, db) {
 
             if (err) { return callback(err); }
 
-            // "md5-32/sha1-40/sha256-64/auto"
-            switch (params.hash) {
-                case "md5":
-                case "sha1":
-                case "sha256":
-                case "sha512":
-                    var hash = crypto.createHash(params.hash);
-                    hash.update(password);
-                    password = hash.digest("hex").toLowerCase();
-                    break;
-                case "none":
-                    break;
-                default:
-                    return callback(params.hash ? "Missing hash algorithm" : "Invalid hash algorithm");
-            }
-
-            var filter = {};
-            filter[params.userkey] = username;
-            filter[params.passkey] = password;
-
-            collection.findOne(filter, function(err, doc) {
+            db.collection(ds.collection, function(err, collection) {
 
                 if (err) { return callback(err); }
 
-                if (!doc) {
-                    return error(ERROR_USER_OR_PASS_NOT_VALID, callback);
+                // "md5-32/sha1-40/sha256-64/auto"
+                switch (params.hash) {
+                    case "md5":
+                    case "sha1":
+                    case "sha256":
+                    case "sha512":
+                        var hash = crypto.createHash(params.hash);
+                        hash.update(password);
+                        password = hash.digest("hex").toLowerCase();
+                        break;
+                    case "none":
+                        break;
+                    default:
+                        return callback(params.hash ? "Missing hash algorithm" : "Invalid hash algorithm");
                 }
 
-                callback(null, doc);
+                var filter = {};
+                filter[params.userkey] = username;
+                filter[params.passkey] = password;
+
+                collection.findOne(filter, function(err, user) {
+
+                    if (err) { return callback(err); }
+
+                    if (!user) {
+                        return callback(error(ERROR_USER_OR_PASS_NOT_VALID));
+                    }
+
+                    callback(null, user);
+                });
             });
         });
     });
 }
 
-function error(code, callback) {
+function error(code) {
     var message = ERRORS[code];
     var err = null;
     if (message) {
@@ -217,7 +163,7 @@ function error(code, callback) {
         err = new Error("Unknown error");
         err.code = 1;
     }
-    callback(err);
+    return err;
 }
 
 var ERROR_USER_OR_PASS_NOT_VALID = 101;
@@ -229,34 +175,4 @@ var ERRORS = {
     "102": "Missing username",
     "103": "Missing password"
 };
-
-function openDatabase(dataSource, callback) {
-
-    if (!dataSource || !dataSource.db) {
-        return callback("Invalid data source.");
-    }
-
-    switch (dataSource.type) {
-
-        case "mongo":
-            // check the cache first maybe we have it already
-            if (databases[dataSource.db]) {
-                callback(null, databases[dataSource.db]);
-                return;
-            }
-
-            // open a new connection to the database
-            var server = new mongo.Server('localhost', 27017, { auto_reconnect: true, poolSize: 5 });
-            var db = new mongo.Db(dataSource.db, server, { w: 0 });
-
-            // cache this db connection
-            databases[dataSource.db] = db;
-
-            db.open(callback);
-            return;
-
-        default:
-            return callback("Invalid data source type: " + dataSource.type);
-    }
-}
 
